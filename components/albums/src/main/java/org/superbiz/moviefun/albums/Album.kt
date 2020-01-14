srinivaskamsani/apps-movie-fun-modernization-code

Convert album component to Kotlin

components/albums/src/main/java/org/superbiz/moviefun/albums/Album.kt
@@ -1,0 +1,50 @@
+package org.superbiz.moviefun.albums
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Album(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+
+    val artist: String? = null,
+    val title: String? = null,
+    val year: Int = 0,
+    val rating: Int = 0
+) {
+
+    fun hasId(): Boolean {
+        return id != null
+    }
+
+    fun isEquivalent(other: Album): Boolean {
+        if (year != other.year) return false
+        if (title != other.title) return false
+        if (artist != other.artist) return false
+
+        return true
+    }
+}

yfujiu/movie-fun-modernization

components/albums/src/main/java/org/superbiz/moviefun/albums/Album.kt
@@ -1,0 +1,50 @@
+package org.superbiz.moviefun.albums
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Album(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    val artist: String? = null,
+    val title: String? = null,
+    val year: Int = 0,
+    val rating: Int = 0
+) {
+
+    fun hasId(): Boolean {
+        return id != null
+    }
+
+    fun isEquivalent(other: Album): Boolean {
+        if (year != other.year) return false
+        if (title != other.title) return false
+        if (artist != other.artist) return false
+
+        return true
+    }
+}